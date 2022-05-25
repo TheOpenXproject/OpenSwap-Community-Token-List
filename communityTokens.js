c967c3b7a0e367b5545b2e05bb1ee031553acf1c
Nauta <22015497+NauCode@users.noreply.github.com>
2022-05-25 15:38:57 +0200

    Added data for EvoVerses

--- communityTokens.js
+++ communityTokens.js
@@ -1,10 +1,10 @@
 export const communitytokens = {
-	MATIC: {
-    Symbol: "pWMATIC", //token symbol
-    name: "Poly Wrapped Matic", //token name
-    oneAddress: "one1l0w3jsmkmcv63qgcap8z0xuh0ut96qdcqsnnxk", //bech32 address
-    oneZeroxAddress: "0xfbdd194376de19a88118e84e279b977f165d01b8", //hex address (0x)
-    imgSrc: "https://s2.coinmarketcap.com/static/img/coins/64x64/3890.png", //imageURL
+	EVO: {
+    Symbol: "EVO", //token symbol
+    name: "EVO", //token name
+    oneAddress: "one1td68ugafunzsnhgxl0fvpc7t3wzxuwv0ranhyn", //bech32 address
+    oneZeroxAddress: "0x5b747e23a9E4c509dd06fbd2c0e3cB8B846e398F", //hex address (0x)
+    imgSrc: "https://evoverses.com/evoToken.png", //imageURL
     decimals: 18 //token decimals
   },
 }
